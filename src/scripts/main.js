522049b5304ef97a965673c75546eebbee9312d9
Roman Rusin <rgromkorusin12@gmail.com>
2024-11-10 18:37:46 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('form input').forEach((input) => {
+  // Створюємо елемент `label`
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  // Встановлюємо атрибут `for` з ідентифікатором `input`
+  label.setAttribute('for', input.id);
+
+  // Встановлюємо текст для `label` на основі `input` `name`
+  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  // Встановлюємо `placeholder` для `input`, капіталізуючи його ім'я
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  // Додаємо `label` до батьківського контейнера `input`
+  input.parentNode.insertBefore(label, input);
+});
